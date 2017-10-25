b2b5ba945fa617e81c18431aa81c80c6d889704e
Katie Park <katie.sang.park@gmail.com>
2017-10-25 00:26:15 -0700

    player movement

--- game/game/Source.c
+++ game/game/Source.c
@@ -1,14 +1,53 @@
-#include <stdio.h>
+#include <windows.h>
 
-enum COUNT {
-	ZERO, 
-	ONE, 
-	TWO	
-};
+static int g_nScreenIndex;
+static HANDLE g_hScreen[2];
 
-int main(void) {
-	enum COUNT nCount;
-	nCount = ONE;
-	printf("%d", nCount);
-	return 0;
+void ScreenInit()
+{
+	CONSOLE_CURSOR_INFO cci;
+
+	
+	g_hScreen[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	g_hScreen[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+	
+	cci.dwSize = 1;
+	cci.bVisible = FALSE;
+	SetConsoleCursorInfo(g_hScreen[0], &cci);
+	SetConsoleCursorInfo(g_hScreen[1], &cci);
+}
+
+void ScreenFlipping()
+{
+	Sleep(10);
+	SetConsoleActiveScreenBuffer(g_hScreen[g_nScreenIndex]);
+	g_nScreenIndex = !g_nScreenIndex;
+}
+
+void ScreenClear()
+{
+	COORD Coor = { 0, 0 };
+	DWORD dw;
+	FillConsoleOutputCharacter(g_hScreen[g_nScreenIndex], ' ', 80 * 25, Coor, &dw);
+}
+
+void ScreenRelease()
+{
+	CloseHandle(g_hScreen[0]);
+	CloseHandle(g_hScreen[1]);
+}
+
+
+void ScreenPrint(int x, int y, char *string)
+{
+	DWORD dw;
+	COORD CursorPosition = { x, y };
+	SetConsoleCursorPosition(g_hScreen[g_nScreenIndex], CursorPosition);
+	WriteFile(g_hScreen[g_nScreenIndex], string, strlen(string), &dw, NULL);
+}
+
+void SetColor(unsigned short color)
+{
+	SetConsoleTextAttribute(g_hScreen[g_nScreenIndex], color);
 }
--- game/game/main.c
+++ game/game/main.c
@@ -1,41 +1,102 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include "Screen.h"
+#include <windows.h>
 #include <time.h>
 
-void Init() {
+typedef struct _PLAYER
+{
+	int nCenterX, nCenterY;
+	int nMoveX, nMoveY;
+	int nX, nY;
+} PLAYER;
 
+PLAYER g_sPlayer;
+char g_strPlayer[] = "----0----";
+int g_nLength;
+
+void Init()
+{
+	g_sPlayer.nCenterX = 4;
+	g_sPlayer.nCenterY = 0;
+	g_sPlayer.nMoveX = 20;
+	g_sPlayer.nMoveY = 22;
+	g_sPlayer.nX = g_sPlayer.nMoveX - g_sPlayer.nCenterX;
+	g_nLength = strlen(g_strPlayer);
 }
-void Update() {
 
+void Update()
+{
 }
-void Render() {
 
+void Render()
+{
+	char string[100] = { 0, };
+	ScreenClear();
+
+	if (g_sPlayer.nX < 0)  
+		ScreenPrint(0, g_sPlayer.nMoveY, &g_strPlayer[g_sPlayer.nX*-1]);
+	else if (g_sPlayer.nMoveX + (g_nLength - (g_sPlayer.nCenterX + 1)) > 79) 
+	{
+		strncat(string, g_strPlayer, g_nLength - ((g_sPlayer.nMoveX + g_sPlayer.nCenterX + 1) - 79));
+		ScreenPrint(g_sPlayer.nX, g_sPlayer.nMoveY, string);
+	}
+	else { 
+		ScreenPrint(g_sPlayer.nX, g_sPlayer.nMoveY, g_strPlayer);
+	}
+
+	sprintf(string, "movement : %d, %d", g_sPlayer.nMoveX, g_sPlayer.nMoveY);
+	ScreenPrint(0, 0, string);
+
+	ScreenFlipping();
 }
-void Release() {
 
+void Release()
+{
 }
 
 int main(void)
 {
+	int nKey, nRemain;
 
-	clock_t CurTime, OldTime;
-	Init();
-	OldTime = clock();
+	ScreenInit();
+	Init();      
 
-	Init();
-	while (1) {
-		OldTime = clock();
-
-		Update();
-		Render();
-
-		while (1)
+	while (1)
+	{
+		if (_kbhit())
 		{
-			CurTime = clock();
-			if (CurTime - OldTime > 33)
+			nKey = _getch();
+			if (nKey == 'q')
 				break;
-		}
-	}
-	Release();
-	return 0;
 
+			switch (nKey)
+			{
+			case 'j':
+				g_sPlayer.nMoveX--;
+				nRemain = g_nLength - (g_sPlayer.nCenterX + 1); 
+				if (g_sPlayer.nMoveX - g_sPlayer.nCenterX < 0 || g_sPlayer.nMoveX + nRemain > 79)
+					g_sPlayer.nMoveX--;
+
+				g_sPlayer.nX = g_sPlayer.nMoveX - g_sPlayer.nCenterX;
+				break;
+			case 'l':
+				g_sPlayer.nMoveX++;
+				nRemain = g_nLength - (g_sPlayer.nCenterX + 1); 
+				if (g_sPlayer.nMoveX + nRemain > 79 || (g_sPlayer.nMoveX - g_sPlayer.nCenterX < 0))
+					g_sPlayer.nMoveX++;
+
+				g_sPlayer.nX = g_sPlayer.nMoveX - g_sPlayer.nCenterX;
+				break;
+			}
+		}
+
+		Update();    
+		Render();    	
+	}
+
+	Release();  
+	ScreenRelease();
+	return 0;
 }
